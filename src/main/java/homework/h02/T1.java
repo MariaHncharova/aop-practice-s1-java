64f2d66740d9321a2cd28e5975c3c7bc0a2dcd24
HoncharovaMaria <honcharova.mariia@e-u.edu.ua>
2026-09-26 18:44:47 +0300

    виконано практичну 2

--- src/main/java/homework/h02/T1.java
+++ src/main/java/homework/h02/T1.java
@@ -1,5 +1,11 @@
 package homework.h02;
 
 // base
-// https://leetcode.com/problems/add-binary/
-public class T1 {}
+// https://leetcode.com
+public class T1 {
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
+    }
+}
--- src/main/java/homework/h02/T2.java
+++ src/main/java/homework/h02/T2.java
@@ -1,5 +1,10 @@
 package homework.h02;
 
-// advanced
-// https://leetcode.com/problems/divide-two-integers/
-public class T2 {}
+// base
+// // https://leetcode.com/problems/divide-two-integers/
+public class T2 {
+    public boolean isSameAfterReversals(int num) {
+        if (num == 0) return true;
+        return num % 10 != 0;
+    }
+}
