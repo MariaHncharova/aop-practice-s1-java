57311d72993526cae126cad56b93880d1b55b179
HoncharovaMaria <honcharova.mariia@e-u.edu.ua>
2026-09-26 20:35:07 +0300

    виконано практичну 3

--- src/main/java/homework/h03/T1.java
+++ src/main/java/homework/h03/T1.java
@@ -2,4 +2,18 @@ package homework.h03;
 
 // base
 // https://leetcode.com/problems/roman-to-integer/
-public class T1 {}
+public class T1 {
+public double average(int[] salary) {
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    int sum = 0;
+
+    for (int s : salary) {
+        if (s < min) min = s;
+        if (s > max) max = s;
+        sum += s;
+    }
+
+    return (double) (sum - min - max) / (salary.length - 2);
+   }
+}
--- src/main/java/homework/h03/T2.java
+++ src/main/java/homework/h03/T2.java
@@ -2,4 +2,19 @@ package homework.h03;
 
 // advanced
 // https://leetcode.com/problems/reverse-integer/
-public class T2 {}
+public class T2 {
+    public int differenceOfSums(int n, int m) {
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % m != 0) {
+                num1 += i;
+            } else {
+                num2 += i;
+            }
+        }
+
+        return num1 - num2;
+    }
+}
